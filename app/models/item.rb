920250f0a462a702f4f0a80edc9808091083ff20
Syota-Yamaguchi <h.10ve.vint.janda@gmail.com>
2020-09-13 12:59:56 +0900

    商品出品機能実装再修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,6 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :user, optional: true
-  #has_one :order
   has_one_attached :image
   belongs_to_active_hash :category
   belongs_to_active_hash :status
