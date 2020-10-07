1ad59ec6cc29329a2b3e02cc4173c08a20055243
Syota-Yamaguchi <h.10ve.vint.janda@gmail.com>
2020-10-07 14:16:10 +0900

    実験３

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :rbenv_ruby, '2.6.5'
 set :ssh_options, auth_methods: ['publickey'],
                                   keys: ['~/.ssh/kakkeru1965.pem']
 
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn,pid" }
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
